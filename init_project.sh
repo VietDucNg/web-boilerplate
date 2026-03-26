abb7b66dfae1a6d9c108d467786620a63a846f85
VietDucNg <vietducnguyen.fit@gmail.com>
2026-03-26 11:50:36 +0100

    build: add init_project.sh

---
+++ init_project.sh
@@ -0,0 +1,11 @@
+# start Node.js project
+npm init -y --init-type=module # yes to all, use ES Modules
+
+# webpack
+npm install --save-dev webpack webpack-cli
+npm install --save-dev html-webpack-plugin
+npm install --save-dev style-loader css-loader
+npm install --save-dev html-loader
+
+# webpack dev server
+npm install --save-dev webpack-dev-server
